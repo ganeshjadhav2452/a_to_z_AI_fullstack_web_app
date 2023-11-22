564147f47563ad8a2b07ce49403b4d65dce62cd1
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-11-22 18:25:34 +0530

    lsdjf

--- server/server.js
+++ server/server.js
@@ -11,9 +11,16 @@ const path = require('path')
 connectDatabase()
 
 
+const corsOptions = {
+    origin: 'https://a-to-z-ai-production-azxe.vercel.app', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable credentials (if needed)
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
+
 //intilizing app
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(errorHandler)
